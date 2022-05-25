a10b65c456701ffb98564cb0ab03f15e0078347d
Rahul kumar dhar dubey <78297145+rkdd51@users.noreply.github.com>
2022-05-25 15:08:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Dropzonee from "./Dropzonee";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dropzone</h1>
+      <Dropzonee />
+    </div>
+  );
+}
